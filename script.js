6f2c40a6778d3f16693e20b913563678152f3fd8
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-03-05 22:25:24 +0100

    pseudo responsive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuIcon=document.querySelector('.hamburger-menu');
 const navbar=document.querySelector(".navbar");
+const body = document.querySelector("body");
 
 const header_logo=document.querySelector(".header-logo");
 
@@ -9,5 +10,6 @@ menuIcon.addEventListener('click', onClick);
 function onClick()
 {
     navbar.classList.toggle('change');
-    header_logo.classList.toggle('change');
+    header_logo.classList.toggle('change-cross');
+    body.classList.toggle('no-scroll');
 }
