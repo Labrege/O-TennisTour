c21d7234d1c0df9f41bc3285633cf7db91b7adfb
Labrege <antoine.brege92@gmail.com>
2021-03-10 12:31:05 +0100

    nouvelle version de l'abonnement

--- formules.php
+++ formules.php
@@ -17,8 +17,7 @@
       ?>
     </header>
     
-    <!-- La Page Index.php -->
-    <div class="rest">
+    
     <div id="espace_membre_abonnement">
 
         <div class="titre-offres">
@@ -34,10 +33,10 @@
             <li><a href="../espace-membre/abonnement/.php"> Suprimer mon compte </a></li>
         </ul> 
         -->
-
+        <!-- 
         <div class="offre_abonement_global">
             <div class="level_plan" >
-                <div class="plan_abonnement"><!-- abonement gratuit-->
+                <div class="plan_abonnement">
                     <div class="titre-formule">
                         <h3>ATP 500</h3>
                         <img src="https://img.icons8.com/emoji/48/000000/3rd-place-medal-emoji.png"/>
@@ -58,7 +57,7 @@
                 </div>
 
 
-                <div class="plan_abonnement"><!-- abonement prenium-->
+                <div class="plan_abonnement">
                     <div class="titre-formule">
                         <h3>ATP 1000</h3>
                         <img src="https://img.icons8.com/emoji/48/000000/2nd-place-medal-emoji.png"/>
@@ -80,7 +79,7 @@
                 </div>
 
                 <div class="plan_abonnement">
-                    <div class="titre-formule"><!-- abonement club-->
+                    <div class="titre-formule">
                         <h3>Grand Chelme</h3>
                         <img src="https://img.icons8.com/emoji/48/000000/1st-place-medal-emoji.png"/>
                     </div>
@@ -105,7 +104,93 @@
 
         </div>
 
+    </div>-->
+
+
+    
+        <div class="container">
+            <div class="card">
+                <div class="titre-formule">
+                    <h2>ATP 500</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    width="48" height="48"
+                    viewBox="0 0 172 172"
+                    style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g><path d="M86,14.33333c-39.58041,0 -71.66667,32.08626 -71.66667,71.66667c0,39.58041 32.08626,71.66667 71.66667,71.66667c39.58041,0 71.66667,-32.08626 71.66667,-71.66667c0,-39.58041 -32.08626,-71.66667 -71.66667,-71.66667z" fill="#e67e22"></path><path d="M157.66667,86c0,0.89583 0,1.79167 -0.07167,2.6875c-3.51167,0.60917 -7.095,0.89583 -10.67833,0.89583c-35.5825,0 -64.5,-28.9175 -64.5,-64.5c0,-3.58333 0.28667,-7.16667 0.89583,-10.67833c0.89583,-0.07167 1.79167,-0.07167 2.6875,-0.07167c1.54083,0 3.04583,0.03583 4.55083,0.17917c-0.645,3.47583 -0.9675,7.02333 -0.9675,10.57083c0,31.605 25.72833,57.33333 57.33333,57.33333c3.5475,0 7.095,-0.3225 10.57083,-0.9675c0.14333,1.505 0.17917,3.01 0.17917,4.55083zM89.58333,146.91667c0,3.58333 -0.28667,7.16667 -0.89583,10.67833c-0.89583,0.07167 -1.79167,0.07167 -2.6875,0.07167c-1.54083,0 -3.04583,-0.03583 -4.55083,-0.17917c0.645,-3.47583 0.9675,-7.02333 0.9675,-10.57083c0,-31.605 -25.72833,-57.33333 -57.33333,-57.33333c-3.5475,0 -7.095,0.3225 -10.57083,0.9675c-0.14333,-1.505 -0.17917,-3.01 -0.17917,-4.55083c0,-0.89583 0,-1.79167 0.07167,-2.6875c3.51167,-0.60917 7.095,-0.89583 10.67833,-0.89583c35.5825,0 64.5,28.9175 64.5,64.5z" fill="#fafafa"></path></g></g></svg>
+                </div>
+                    
+                <div class="prix_par_mois">100€</div>
+                <div class="contenue_propose">
+                    <ul class="liste_contenu_plan">
+                        <li class="contenu_plan"><img src="Images/tick.png" alt=""> <p>court de tennis </p></li>
+                        <li class="contenu_plan"> <img src="Images/tick.png" alt=""><p>activités multisport</p> </li>
+                        <li class="contenu_plan"> <img src="Images/tick.png" alt=""><p>logement</p></li>
+                    </ul>
+                </div>
+                
+                <div class="boite-bouton-valider">
+                    <button type="button_gratuit" class="bouton_valider" name="button_gratuit">Choisir ce plan</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <div class="titre-formule">
+                    <h2>ATP 1000</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    width="48" height="48"
+                    viewBox="0 0 172 172"
+                    style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g><path d="M86,14.33333c-39.58041,0 -71.66667,32.08626 -71.66667,71.66667c0,39.58041 32.08626,71.66667 71.66667,71.66667c39.58041,0 71.66667,-32.08626 71.66667,-71.66667c0,-39.58041 -32.08626,-71.66667 -71.66667,-71.66667z" fill="#cccccc"></path><path d="M157.66667,86c0,0.89583 0,1.79167 -0.07167,2.6875c-3.51167,0.60917 -7.095,0.89583 -10.67833,0.89583c-35.5825,0 -64.5,-28.9175 -64.5,-64.5c0,-3.58333 0.28667,-7.16667 0.89583,-10.67833c0.89583,-0.07167 1.79167,-0.07167 2.6875,-0.07167c1.54083,0 3.04583,0.03583 4.55083,0.17917c-0.645,3.47583 -0.9675,7.02333 -0.9675,10.57083c0,31.605 25.72833,57.33333 57.33333,57.33333c3.5475,0 7.095,-0.3225 10.57083,-0.9675c0.14333,1.505 0.17917,3.01 0.17917,4.55083zM89.58333,146.91667c0,3.58333 -0.28667,7.16667 -0.89583,10.67833c-0.89583,0.07167 -1.79167,0.07167 -2.6875,0.07167c-1.54083,0 -3.04583,-0.03583 -4.55083,-0.17917c0.645,-3.47583 0.9675,-7.02333 0.9675,-10.57083c0,-31.605 -25.72833,-57.33333 -57.33333,-57.33333c-3.5475,0 -7.095,0.3225 -10.57083,0.9675c-0.14333,-1.505 -0.17917,-3.01 -0.17917,-4.55083c0,-0.89583 0,-1.79167 0.07167,-2.6875c3.51167,-0.60917 7.095,-0.89583 10.67833,-0.89583c35.5825,0 64.5,28.9175 64.5,64.5z" fill="#fafafa"></path></g></g></svg>
+                </div>
+                    
+                <div class="prix_par_mois">100€</div>
+                <div class="contenue_propose">
+                    <ul class="liste_contenu_plan">
+                        <li class="contenu_plan"><img src="Images/tick.png" alt=""> <p>court de tennis </p></li>
+                        <li class="contenu_plan"> <img src="Images/tick.png" alt=""><p>activités multisport</p> </li>
+                        <li class="contenu_plan"> <img src="Images/tick.png" alt=""><p>logement</p></li>
+                    </ul>
+                </div>
+                
+                <div class="boite-bouton-valider">
+                    <button type="button_gratuit" class="bouton_valider" name="button_gratuit">Choisir ce plan</a>
+                </div>
+                
+            </div>
+            <div class="card">
+                <div class="titre-formule">
+                    <h2>Grand Shleme</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    width="48" height="48"
+                    viewBox="0 0 172 172"
+                    style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g><path d="M86,14.33333c-39.58041,0 -71.66667,32.08626 -71.66667,71.66667c0,39.58041 32.08626,71.66667 71.66667,71.66667c39.58041,0 71.66667,-32.08626 71.66667,-71.66667c0,-39.58041 -32.08626,-71.66667 -71.66667,-71.66667z" fill="#f1c40f"></path><path d="M157.66667,86c0,0.89583 0,1.79167 -0.07167,2.6875c-3.51167,0.60917 -7.095,0.89583 -10.67833,0.89583c-35.5825,0 -64.5,-28.9175 -64.5,-64.5c0,-3.58333 0.28667,-7.16667 0.89583,-10.67833c0.89583,-0.07167 1.79167,-0.07167 2.6875,-0.07167c1.54083,0 3.04583,0.03583 4.55083,0.17917c-0.645,3.47583 -0.9675,7.02333 -0.9675,10.57083c0,31.605 25.72833,57.33333 57.33333,57.33333c3.5475,0 7.095,-0.3225 10.57083,-0.9675c0.14333,1.505 0.17917,3.01 0.17917,4.55083zM89.58333,146.91667c0,3.58333 -0.28667,7.16667 -0.89583,10.67833c-0.89583,0.07167 -1.79167,0.07167 -2.6875,0.07167c-1.54083,0 -3.04583,-0.03583 -4.55083,-0.17917c0.645,-3.47583 0.9675,-7.02333 0.9675,-10.57083c0,-31.605 -25.72833,-57.33333 -57.33333,-57.33333c-3.5475,0 -7.095,0.3225 -10.57083,0.9675c-0.14333,-1.505 -0.17917,-3.01 -0.17917,-4.55083c0,-0.89583 0,-1.79167 0.07167,-2.6875c3.51167,-0.60917 7.095,-0.89583 10.67833,-0.89583c35.5825,0 64.5,28.9175 64.5,64.5z" fill="#fafafa"></path></g></g></svg>
+                </div>
+                    
+                <div class="prix_par_mois">100€</div>
+                <div class="contenue_propose">
+                    <ul class="liste_contenu_plan">
+                        <li class="contenu_plan"><img src="Images/tick.png" alt=""> <p>court de tennis </p></li>
+                        <li class="contenu_plan"> <img src="Images/tick.png" alt=""><p>activités multisport</p> </li>
+                        <li class="contenu_plan"> <img src="Images/tick.png" alt=""><p>logement</p></li>
+                    </ul>
+                </div>
+                
+                <div class="boite-bouton-valider">
+                    <button type="button_gratuit" class="bouton_valider" name="button_gratuit">Choisir ce plan</a>
+                </div>
+                
+            </div>
+        </div>
+    
+
     </div>
+<script type="text/javascript" src="JS/vanilla-tilt.js"></script>
+    <script>
+        VanillaTilt.init(document.querySelectorAll(".card"),{
+            max:25,
+            speed:400,
+            //glare: true,
+            //"max-glare":1,
+        });
+    </script>
     <script src="JS/script.js"></script>
 </body>
 </html>
