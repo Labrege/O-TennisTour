0d1ff814831106f1f714a42bb93fd08e6e090051
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-08-15 11:43:39 +0200

    15/08

--- infrastructures.php
+++ infrastructures.php
@@ -26,6 +26,135 @@
                 <div class="image-card">
                     <img src="Images/creps-poitier.jpeg" alt="">
                 </div>
+                <div class="titre-card">
+                    <h2> Site de Poitier </h2>
+                </div>
+                <div class="texte-card">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem omnis architecto praesentium id porro eum nesciunt nihil temporibus. Maxime totam quaerat, veniam recusandae nostrum nesciunt, minima natus quod architecto porro corporis tempore? Repellat inventore laudantium voluptate quasi suscipit sed. Dicta commodi vitae magni ea, velit assumenda culpa labore tempora id esse consequuntur voluptate praesentium qui beatae, quae minima ipsa nesciunt?</p>
+                </div>
+                <div class="sous-titre-card">
+                    <h2> Les activités </h2>
+                </div>
+                <div class="sous-card-container">
+                    <div class="sous-card">
+                        <div class="image-sous-card">
+                            <img src="Images/tir-arc.jpeg" alt="">
+                        </div>
+                        <div class="sous-card-content">
+                            <div class="titre-sous-card">
+                                <h3> Tir à l'arc</h3>
+                            </div>
+                            <div class="text-sous-card">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur nostrum velit molestias beatae quod cum sapiente molestiae sint corrupti.
+                                </p>
+                            </div>
+                            <div class="boite-bouton-valider">
+                                <button type="button_gratuit" class="bouton_valider" name="button_gratuit">En savoir plus</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sous-card">
+                        <div class="image-sous-card">
+                            <img src="Images/futuroscope.jpeg" alt="">
+                        </div>
+                        <div class="sous-card-content">
+                            <div class="titre-sous-card">
+                                <h3> Futuroscope</h3>
+                            </div>
+                            <div class="text-sous-card">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur nostrum velit molestias beatae quod cum sapiente molestiae sint corrupti.
+                                </p>
+                            </div>
+                            <div class="boite-bouton-valider">
+                                <button type="button_gratuit" class="bouton_valider" name="button_gratuit">En savoir plus</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sous-card">
+                        <div class="image-sous-card">
+                            <img src="Images/escalade.jpeg" alt="">
+                        </div>
+                        <div class="sous-card-content">
+                            <div class="titre-sous-card">
+                                <h3> escalade</h3>
+                            </div>
+                            <div class="text-sous-card">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur nostrum velit molestias beatae quod cum sapiente molestiae sint corrupti.
+                                </p>
+                            </div>
+                            <div class="boite-bouton-valider">
+                                <button type="button_gratuit" class="bouton_valider" name="button_gratuit">En savoir plus</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="image-card">
+                    <img src="Images/creps-poitier.jpeg" alt="">
+                </div>
+                <div class="titre-card">
+                    <h2> Site de Poitier </h2>
+                </div>
+                <div class="texte-card">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem omnis architecto praesentium id porro eum nesciunt nihil temporibus. Maxime totam quaerat, veniam recusandae nostrum nesciunt, minima natus quod architecto porro corporis tempore? Repellat inventore laudantium voluptate quasi suscipit sed. Dicta commodi vitae magni ea, velit assumenda culpa labore tempora id esse consequuntur voluptate praesentium qui beatae, quae minima ipsa nesciunt?</p>
+                </div>
+                <div class="sous-titre-card">
+                    <h2> Les activités </h2>
+                </div>
+                <div class="sous-card-container">
+                    <div class="sous-card">
+                        <div class="image-sous-card">
+                            <img src="Images/tir-arc.jpeg" alt="">
+                        </div>
+                        <div class="sous-card-content">
+                            <div class="titre-sous-card">
+                                <h3> Tir à l'arc</h3>
+                            </div>
+                            <div class="text-sous-card">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur nostrum velit molestias beatae quod cum sapiente molestiae sint corrupti.
+                                </p>
+                            </div>
+                            <div class="boite-bouton-valider">
+                                <button type="button_gratuit" class="bouton_valider" name="button_gratuit">En savoir plus</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sous-card">
+                        <div class="image-sous-card">
+                            <img src="Images/futuroscope.jpeg" alt="">
+                        </div>
+                        <div class="sous-card-content">
+                            <div class="titre-sous-card">
+                                <h3> Futuroscope</h3>
+                            </div>
+                            <div class="text-sous-card">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur nostrum velit molestias beatae quod cum sapiente molestiae sint corrupti.
+                                </p>
+                            </div>
+                            <div class="boite-bouton-valider">
+                                <button type="button_gratuit" class="bouton_valider" name="button_gratuit">En savoir plus</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sous-card">
+                        <div class="image-sous-card">
+                            <img src="Images/escalade.jpeg" alt="">
+                        </div>
+                        <div class="sous-card-content">
+                            <div class="titre-sous-card">
+                                <h3> escalade</h3>
+                            </div>
+                            <div class="text-sous-card">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur nostrum velit molestias beatae quod cum sapiente molestiae sint corrupti.
+                                </p>
+                            </div>
+                            <div class="boite-bouton-valider">
+                                <button type="button_gratuit" class="bouton_valider" name="button_gratuit">En savoir plus</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     
