0824118800223b2b401e06ce64ade7f69cc51bb2
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-09-23 23:00:36 +0200

    favicon

--- links.php
+++ links.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
     <link rel="stylesheet" href="CSS/header.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/formules.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/footer.css?v=<?php echo time(); ?>">
@@ -13,11 +14,21 @@
     <link rel="stylesheet" href="CSS/inscriptions.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/equipe.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/infrastructures.css?v=<?php echo time(); ?>">
+    <!-- Links -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <title>O-Tennis Tour | Tennis Academy </title>
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <!-- SEO -->
+    <title> Academie de tennis à Paris | O'Tennis Tour </title>
+    <meta name="description" content="Envie de jouer au tennis sur Paris ? Découvrez et progressez rapidement au tennis avec l'académie O'Tennis Tour. Choississez parmis nos différentes options et commencez dès maintenant !">
+    <meta name="keywords" content="Tennis, Tennis Paris, Coaching, TENNIS, TENNIS PARIS, O Tennis Tour, OTT, otennistour, o-tennis tour, O'Tennis Tour, Leçon individuelle, Roland Garros, Club Paris, Académie, Tennis Academy, Académie de tennis, Academy">
 </head>
