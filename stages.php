5b863d113dae4f15866490a7d6ab82bdccc0f6e8
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-10-30 23:40:22 +0200

    30

---
+++ stages.php
@@ -0,0 +1,135 @@
+<header>
+    <?php
+        require 'header.php'
+    ?>
+</header>
+
+<div class="rest">
+    <div id='espace_membre_stages' class="espace_membre_stages">
+
+        <!-- Début intro stages -->
+        <div class="intro-stage-container">
+            <img class='intro-background-image' src="Images/tennis-net.jpg" alt="">
+            <div class="intro-stage-screen"></div>
+            <div class="intro-text-background">
+                <h1> LES STAGES O'TENNIS TOUR </h1>
+                <div class="intro-text-container">
+                    <div class="intro-text">
+                        <img class='intro-text-image' src="Images/tennis-kid.jpg" alt="">
+                    </div>
+                    <div class="intro-text">
+                        <h2> UN ÉTÉ HORS DU COMMUN : OFFREZ-LUI UN STAGE DE TENNIS EN 2021 ! </h2>
+                        <p> Nous proposons différents programmes d’entraînement adaptés pour améliorer le niveau physique & tennistique des joueurs, des tournois officiels ainsi que des cours de langues intensifs (selon options). Les stages peuvent être pratiqués par tous, pour les débutants comme pour les compétiteurs à partir de 6 ans.
+                        <br>
+                        Les stages sont proposés :
+                        <br>
+                        <br>
+                        
+                        <li> sur une durée d’une semaine </li>
+                        <li>à partir de 1h30 de tennis par jour</li>
+                        <li>à partir de 1h30 de fitness par jour</li>
+                        <li>par groupes de niveau de 4 joueurs par terrain</li>
+                        <li>avec ou sans hébergement (sur place dans des chambres triples ou quadruples)</li>
+                        <li>avec repas inclus (petit-déjeuner, déjeuner et dîner)</li>
+                        <br>
+                        
+                        Cette année, les stages de tennis d’été commencent à partir du 14 juin 2021 jusqu’au 30 aout 2021. Venez découvrir ou redécouvrir la Mouratoglou Academy et choisissez le stage qui correspond le mieux à vos attentes.
+                        </p>
+                    </div>
+                </div>
+                <br>
+                <a href="inscriptions.php" class="btn-link">
+                <div class="button-container">
+                    <h2> Réserver un stage </h2>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Début section stages -->
+        <!-- OTT MULTISPORT -->
+        <div class="stage-container">
+           <div class="stage-text-container">
+               <img src="Images/coach.jpg" alt="">
+           </div>
+           <div class="stage-text-container">
+               <div class="stage-text">
+               <h2> STAGE MULTISPORTS </h2>
+               <div class="age-container">
+                   <h3> 10 à 17 ans</h3>
+               </div>
+               <p>Le STAGE MULTI-SPORT est parfait pour les joueurs souhaitant progresser et pratiquer le tennis mais également d'autres sports !</p>
+               <h4> Au programme </h4>
+               <p> Tennis // Padel-Tennis // VTT // Escalade 
+                   <br>
+                   Tir à l'arc // Mini-Golf // Escape Game</p>
+               
+               <h4> Volume d'entraînement </h4>
+               <ul>
+                   <li> Multisport (sports collectifs ) : 9h/semaine</li>
+                   <li> VTT: 1h30/semaine </li>
+                   <li> Tennis : 15h/semaine </li>
+                </ul>
+               </div>
+               <div class="stage-button-container">
+                    <a  class="stage-button" href="#"> M'inscrire au stage</a>
+                    <a  class="stage-button-hollow" target='_blank' href="PDF/brochure.pdf"> Télécharger la brochure </a>
+                </div> 
+           </div>
+           
+        </div>
+        <!-- OTT 500 -->
+        <div class="stage-container">
+           <div class="stage-text-container">
+               <img src="Images/tennis-coach.jpg" alt="">
+           </div>
+           <div class="stage-text-container">
+               <div class="stage-text">
+               <h2> STAGE OTT 500 </h2>
+               <div class="age-container">
+                   <h3> 10 à 17 ans</h3>
+               </div>
+               <p>Améliorez votre tennis et votre condition physique tout en participant à des activités ludiques et culturelles avec la formule OTT 500. Tout au long de la semaine, travail complet sur les fondamentaux du jeu.</p>
+               <h4> Au programme </h4>
+               <p> Une journée sera consacrée à une sortie éducative ou culturelle (ex: Cité de l’espace, Futuroscope…) </p>
+               
+               <h4> Volume d'entraînement </h4>
+               <ul>
+                   <li> Tennis : 15h/semaine </li>
+                   <li> Entraînement physique et sports collectifs : 10H/semaine </li>
+                </ul>
+               </div>
+               <div class="stage-button-container">
+                    <a  class="stage-button" href="#"> M'inscrire au stage</a>
+                    <a  class="stage-button-hollow" target='_blank' href="PDF/brochure.pdf"> Télécharger la brochure </a>
+                </div> 
+           </div>
+           
+        </div>
+        <!-- OTT 1000 -->
+        <div class="stage-container">
+           <div class="stage-text-container">
+               <img src="Images/tennis-coach.jpg" alt="">
+           </div>
+           <div class="stage-text-container">
+               <div class="stage-text">
+               <h2> STAGE OTT 1000 </h2>
+               <div class="age-container">
+                   <h3> 10 à 17 ans</h3>
+               </div>
+               <p>La formule OTT 1000 est le stage parfait pour les joueurs souhaitant commencer la compétition et progresser sur leur jeu en tournoi !</p>
+               <h4> Volume d'entraînement </h4>
+               <ul>
+                   <li> Tennis : 15h/semaine </li>
+                   <li> Entraînement physique et sports collectifs : 10H/semaine </li>
+                </ul>
+               </div>
+               <div class="stage-button-container">
+                    <a  class="stage-button" href="#"> M'inscrire au stage</a>
+                    <a  class="stage-button-hollow" target='_blank' href="PDF/brochure.pdf"> Télécharger la brochure </a>
+                </div> 
+           </div>
+           
+        </div>
+    </div>
+</div>
