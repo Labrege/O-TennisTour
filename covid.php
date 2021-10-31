1c14b13bef077c80e62106df5e4b13c728c171dc
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-10-31 11:42:32 +0100

    covid

---
+++ covid.php
@@ -0,0 +1,48 @@
+<header>
+    <?php
+        require 'header.php'
+    ?>
+</header>
+
+<div class="rest">
+    <div id='espace_membre_stages' class="espace_membre_stages">
+
+        <!-- Début intro stages -->
+        <div class="intro-stage-container">
+            <img class='intro-background-image' src="Images/filet.jpg" alt="">
+            <div class="intro-stage-screen"></div>
+            <div class="intro-text-background">
+                <h1> INFORMATION COVID-19 </h1>
+                <div class="intro-text-container">
+                    <div class="intro-text">
+                        <img class='intro-text-image' src="Images/covid.jpg" alt="">
+                    </div>
+                    <div class="intro-text">
+                        <h2> Les mesures sanitaires prises par O'Tennis Tour ! </h2>
+                        <p> Chez O'Tennis Tour, votre santé et celle de nos joueurs est notre priorité. Nous faisons tout pour garantir cela sur le terrain comme en dehors.
+                        
+                        <h4>Votre responsabilité de joueur !</h4>
+                        
+                        Afin de pouvoir profiter de l'expérience O'Tennis Tour sans crainte, merci de respecter les règles suivantes :
+                        
+                        <h4> Pour les leçons individuelles :</h4>
+                        <li> Utilisation de gel hydroalccolique régulièrement pendant la séance </li>
+                        <li> En cas de symptômes de la Covid-19, effectuer un test PCR ou antigénique avant de reprendre une réservation</li>
+                        <li> En cas de test positif à la COVID-19 avant la leçon, ne pas vous déplacer pour votre cours et <a href="contact.php">contactez-nous</a> </li>
+                        <li> En cas de test positif à la COVID-19 après la leçon, <a href="contact.php">contactez-nous</a> afin de nous permettre de pouvoir prendre les mesures nécessaires</li>
+
+                        <h4> Pour les stages :</h4>
+                        <li> Pour les enfants de 12 ans et plus, fournir un passe sanitaire valide par email avant le début du stage (<a href="contact.php">contactez-nous</a>) </li>
+                        </p>
+                    </div>
+                </div>
+                <br>
+                <a href="https://www.gouvernement.fr/info-coronavirus" target='_blank' class="btn-link">
+                <div class="button-container">
+                    <h2> Plus sur la COVID-19</h2>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
