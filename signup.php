b2e8b5adae8c01fa97cfb99a5129c29120aaeb2c
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-10-31 17:48:12 +0100

    test

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="Text/css" href="CSS/Inscription/inscription.css?v=<?php echo time(); ?>">
-    <title> OvalXV | Inscription</title>
+    <title> O'Tennis Tour | Inscription</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script>
