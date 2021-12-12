c4881cf861454d40287255332466e23d7185823e
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-12 02:05:03 +0100

    Update success-payement.php

--- success-payement.php
+++ success-payement.php
@@ -51,7 +51,7 @@ if(isset($_GET['name']) && isset($_GET['surname']) && isset($_GET['mail']) && is
         $mailSubjectProf = "Nouvelle réservation OTT pour $prof";
         $successUrl = 'index.php';
         $failUrl = 'index.php?error=failedemail';
-        echo $recipients = array('contact@otennistour.com',$mail,$profEmail);
+        print_r($recipients = array('contact@otennistour.com',$mail,$profEmail));
 
         SendEmail($mailFrom, $fromName, $recipients , $mailText, $mailSubjectProf, $successUrl, $failUrl);
         header("Location: success-payement-page.php?amount=$amount&name=$prénom&surname=$nom&tel=$téléphone&mail=$mail&condition=$condition&surface=$surface&age=$age&localisation1=$localisation1&localisation2=$localisation2&prof=$prof&statutprof=$statutprof&time=$heure&date=$date");
