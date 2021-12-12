1cdeba341d24b9d29327cbd9a74a9da713ab6208
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-12 01:50:06 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,11 +7,10 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 
 function SendEmail($mailFrom, $fromName, array $recipients, $mailText, $mailSubject, $successUrl, $failUrl){
-    echo $recipients;
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = false;//SMTP::DEBUG_SERVER;                   // Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;     //false              // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
