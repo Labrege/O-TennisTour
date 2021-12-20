d510ec8dbe3879884013483a19bf325e19d0f672
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-20 22:45:48 +0100

    z

--- functions.php
+++ functions.php
@@ -24,13 +24,13 @@ function SendEmail($mailFrom, $fromName, $recipients, $mailText, $mailSubject, $
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = 1;                   // Enable verbose debug output
+        $mail->SMTPDebug = true;                   // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.hostinger.fr';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'contact@otennistour.com';                     // SMTP username
         $mail->Password   = 'Ott75016';                               // SMTP password
-        $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
