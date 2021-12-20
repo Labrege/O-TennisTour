f04b12189d69adba5d19bace627905c499fbc5b1
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-20 03:10:28 +0100

    Update success-payement.php

--- success-payement.php
+++ success-payement.php
@@ -54,7 +54,7 @@ if(isset($_GET['name']) && isset($_GET['surname']) && isset($_GET['mail']) && is
         <br><br>
         L’équipe O'Tennis Tour ";
         $mailSubjectProf = "Nouvelle réservation OTT pour $prof";
-        $successUrl = "success-payement-page.php?amount=$amount&name=$prénom&surname=$nom&tel=$téléphone&mail=$mail&condition=$condition&surface=$surface&age=$age&localisation1=$localisation1&localisation2=$localisation2&prof=$prof&statutprof=$statutprof&time=$heure&date=$date&idCommande=$idReservation";
+        $successUrl = "success-payement-page.php?amount=$amount&name=$prénom&surname=$nom&tel=$téléphone&condition=$condition&surface=$surface&age=$age&localisation1=$localisation1&localisation2=$localisation2&prof=$prof&statutprof=$statutprof&time=$heure&date=$date&idCommande=$idReservation";
         $failUrl = 'index.php?error=failedemail'; 
         
         $mail = new PHPMailer(true);
