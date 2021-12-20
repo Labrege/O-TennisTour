ad0593c6a8ff21f07bce2fb5bf727fe2bbd7c769
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-20 02:46:54 +0100

    e

--- success-payement.php
+++ success-payement.php
@@ -39,6 +39,7 @@ if(isset($_GET['name']) && isset($_GET['surname']) && isset($_GET['mail']) && is
 
     // Mail //
     if($sql && $sqlChange && $sqlModifyHours){
+        echo 'bite';
         //Expéditeur
         $mailFrom = 'contact@otennistour.com';
         $fromName = "O'TENNIS TOUR";
