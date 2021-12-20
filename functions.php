09b1252292c96134714ada0abf1a4628d5f39c1a
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-20 02:42:32 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,8 +14,8 @@ function SendEmail($mailFrom, $fromName, $recipients, $mailText, $mailSubject, $
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'contact.otennistour@gmail.com';                     // SMTP username
-        $mail->Password   = 'Ott75016';                               // SMTP password
+        $mail->Username   = 'solalpont@gmail.com';                     // SMTP username
+        $mail->Password   = 'Sp23/11/00';                               // SMTP password
         $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;           // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
