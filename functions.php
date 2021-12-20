60fd1f2a57fb2884d8719b3955c7bbd1ad815c73
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-20 02:52:37 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@ function SendEmail($mailFrom, $fromName, $recipients, $mailText, $mailSubject, $
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        echo $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   // Enable verbose debug output
+        // $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.hostinger.fr';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'contact@otennistour.com';                     // SMTP username
