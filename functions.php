49554af56a38c368fe22eaf216703e2aa5961b43
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-20 02:59:12 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function SendEmail($mailFrom, $fromName, $recipients, $mailText, $mailSubject, $
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = 2;                   // Enable verbose debug output
+        echo $mail->SMTPDebug = 2;                   // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.hostinger.fr';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
