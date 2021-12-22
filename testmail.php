310e0fd5688fbded78b921497bf6c53b36df7d53
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-22 21:21:55 +0100

    Update testmail.php

--- testmail.php
+++ testmail.php
@@ -1,11 +1,24 @@
 <?php
-ini_set( 'display_errors', 1);
-errot_reporting( E_ALL );
-$from = "contact@otennistour.com";
-$to ="solalpont@gmail.com";
-$subject = "Vérification PHP Mail";
-$message = "PHP mail marche";
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "L'email a été envoyé.";
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php';
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = 'smtp.hostinger.fr';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->Username = 'contact@otennistour.com';
+$mail->Password = 'Ott75016';
+$mail->setFrom('contact@otennistour.com', 'Votre nom');
+$mail->addReplyTo('contact@otennistour.fr', 'Votre nom');
+$mail->addAddress('solalpont@gmail.com', 'Nom du destinataire');
+$mail->Subject = 'Essai de PHPMailer';
+$mail->msgHTML(file_get_contents('message.html'), __DIR__);
+$mail->Body = 'Ceci est le contenu du message en texte clair';
+//$mail->addAttachment('test.txt');
+if (!$mail->send()) {
+    echo 'Erreur de Mailer : ' . $mail->ErrorInfo;
+} else {
+    echo 'Le message a été envoyé.';
+}
 ?>
