238d9d27cd82f5f8a2cddb959d716182b5055cab
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-29 22:54:42 +0100

    text

--- equipe.php
+++ equipe.php
@@ -1,6 +1,7 @@
 <header>
     <?php
-        require 'header.php'
+        require 'header.php';
+        require 'includes/dbh.inc.php';
     ?>
 </header>
 
@@ -46,22 +47,22 @@
                         </ul>
                     </div>
                 </div>
-                
+
                 <div class="card-liste">
                     <div class="img-coach">
-                        <img src="Images/coachs/alexandre.jpg" alt="">
+                        <img src="Images/coachs/kyrian.jpg" alt="">
                     </div>
                     <div class="card-liste-contenu">
-                        <h3>Alexandre SIDORENKO</h3>
+                        <h3>Kyrian JACQUET</h3>
                         <div class="ligne"></div>
                         <ul>
-                            <li> <i class="fa fa-medal" aria-hidden="true"></i>Classement - (ex) 145e ATP</li>
+                            <li> <i class="fa fa-medal" aria-hidden="true"></i>Classement - 300e ATP</li>
                             <li> Droitier // Revers à deux mains </li>
                             <br>
                             <li>
-                            En 2006, il remporte <span class="red" style='color: #F46E54;'>l'Open d'Australie junior</span> en simple.
-                            Durant sa carrière, Alexandre a gagné quatre tournois Future en simple et huit en doubles ainsi que trois sur le circuit Challenger.
-                            Il a été huitième de finaliste en double à deux reprises  <span class="red" style='color: #F46E54;'>à Roland-Garros.</span>
+                            Kyrian est un des espoirs du tennis français. Toujours en activité sur le circuit, il ne cesse de progresser d'année en année ! 
+                            Il compte déjà dans son palmarès plusieurs <span class="red" style='color: #F46E54;'> tournois Futurs </span> (simple et double) et deux quart de finales en <span class="red" style='color: #F46E54;'> Challenger</span>! 
+                            Il a également participé aux éditions 2019 et 2020 de <span class="red" style='color: #F46E54;'> Roland-Garros.</span>
                             </li>
                         </ul>
                     </div>
@@ -69,31 +70,57 @@
 
                 <div class="card-liste">
                     <div class="img-coach">
-                        <img src="Images/coachs/alexandre.jpg" alt="">
+                        <img src="Images/coachs/martin.jpg" alt="">
                     </div>
                     <div class="card-liste-contenu">
-                        <h3>Alexandre SIDORENKO</h3>
+                        <h3>Martin BREYSACH</h3>
                         <div class="ligne"></div>
                         <ul>
-                            <li> <i class="fa fa-medal" aria-hidden="true"></i>Classement - (ex) 145e ATP</li>
+                            <li> <i class="fa fa-medal" aria-hidden="true"></i>Classement - 821e ATP</li>
                             <li> Droitier // Revers à deux mains </li>
                             <br>
                             <li>
-                            En 2006, il remporte <span class="red" style='color: #F46E54;'>l'Open d'Australie junior</span> en simple.
-                            Durant sa carrière, Alexandre a gagné quatre tournois Future en simple et huit en doubles ainsi que trois sur le circuit Challenger.
-                            Il a été huitième de finaliste en double à deux reprises  <span class="red" style='color: #F46E54;'>à Roland-Garros.</span>
+                            Excellent chez les juniors <span class="red" style='color: #F46E54;'>(19ème mondial en 2020)</span>, Martin commence fort sa transition chez les adultes ! Actuellement à son meilleur classement ATP, 
+                            le jeune français s'illustre sur le circuit Futur en remportant déjà <span class="red" style='color: #F46E54;'>deux titres à Monastir </span>(simple et doubles).
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-        
+            <br>
             <h2> Les coachs O'TENNIS TOUR</h2>
             <p>Chez O’TENNIS TOUR, nous avons souhaité travailler uniquement avec des coachs partageant la même philosophie que nous. Nos expériences tennistiques et personnelles différentes nous amènent à nous compléter pour la réalisation de vos projets.
             <br><br>
-            Tous nos coachs OTT sont titulaires du DEJEPS ou DESJEPS et prêt à vous aider à remplir vos objectifs !</p>
+            Tous nos coachs OTT sont titulaires du DEJEPS ou DESJEPS et prêts à vous aider à remplir vos objectifs !</p>
             <div class="card-liste-container">
+            <?php
+            $sqlCoach = "SELECT * FROM users WHERE userStatut='coach'";
+            $searchCoach = $conn->query($sqlCoach);
 
+            if($searchCoach->num_rows > 0){
+                //Tant qu'il y a des résultat dans la table, afficher...
+                while ($donnees = $searchCoach->fetch_assoc()){
+                    ?>
+                    <div class="card-liste">
+                        <div class="img-coach">
+                            <img src="Images/coachs/<?php echo $donnees['userPhoto'];?>" alt="">
+                        </div>
+                        <div class="card-liste-contenu">
+                            <h3><?php echo $donnees['userSurname'];?> <?php echo $donnees['userName'];?></h3>
+                            <div class="ligne"></div>
+                            <ul>
+                                <li> <i class="fa fa-medal" aria-hidden="true"></i>Classement - <?php echo $donnees['userClassement'];?></li>
+                                <li> <?php echo $donnees['userMain'];?> // <?php echo $donnees['userRevers'];?> </li>
+                                <br>
+                                <li>  <?php echo $donnees['userDescription'];?> </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        
             </div>
             <br>
             <br>
