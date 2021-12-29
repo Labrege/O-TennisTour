c0ab2184d646ce3c8bc1d10afb02d9fb98f0ea05
mevla124 <65461902+mevla124@users.noreply.github.com>
2021-12-29 19:25:10 +0100

    nouveau front

---
+++ JS/page-changer.js
@@ -0,0 +1,296 @@
+var i = 0;
+var btnPrev = $('.btn--prev');
+var btnNext = $('.btn--next');
+var img1 = $('.img1');
+var img2 = $('.img2');
+var img3 = $('.img3');
+var img4 = $('.img4');
+var img5 = $('.img5');
+var vc1 = $('.valeurs-container-1');
+var vc2 = $('.valeurs-container-2');
+var vc3 = $('.valeurs-container-3');
+var vc4 = $('.valeurs-container-4');
+var vc5 = $('.valeurs-container-5');
+var paginationWrapper = $('.pagination-wrapper');
+
+btnPrev.click(function(){
+    i--;
+    if(i<0){
+        i=4;
+    }
+    var page = i+1;
+    $('.carrousel-number').text('0'+page+ ' / 05');
+    paginationWrapper.addClass('transition-prev');
+    if(i==0){
+        img1.addClass('display');
+        img1.removeClass('no-display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc1.addClass('display');
+        vc1.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+        
+    }
+
+    if(i==1){
+        img2.addClass('display');
+        img2.removeClass('no-display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc2.addClass('display');
+        vc2.removeClass('no-display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+    }
+
+    if(i==2){
+        img3.addClass('display');
+        img3.removeClass('no-display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc3.addClass('display');
+        vc3.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+    }
+
+    if(i==3){
+        img4.addClass('display');
+        img4.removeClass('no-display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc4.addClass('display');
+        vc4.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+        
+    }
+
+    if(i==4){
+        img5.addClass('display');
+        img5.removeClass('no-display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+
+        vc5.addClass('display');
+        vc5.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');   
+    }
+
+    var timeout = setTimeout(cleanClasses, 500);
+
+});
+
+btnNext.click(function(){
+    i++;
+    if(i>4){
+        i=0;
+    }
+    var page = i+1;
+    $('.carrousel-number').text('0'+page+ ' / 05');
+    paginationWrapper.addClass('transition-next');
+
+    if(i==0){
+        img1.addClass('display');
+        img1.removeClass('no-display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc1.addClass('display');
+        vc1.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+        
+    }
+
+    if(i==1){
+        img2.addClass('display');
+        img2.removeClass('no-display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc2.addClass('display');
+        vc2.removeClass('no-display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+    }
+
+    if(i==2){
+        img3.addClass('display');
+        img3.removeClass('no-display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc3.addClass('display');
+        vc3.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+    }
+
+    if(i==3){
+        img4.addClass('display');
+        img4.removeClass('no-display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+        img5.addClass('no-display');
+        img5.removeClass('display');
+
+        vc4.addClass('display');
+        vc4.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');
+        vc5.addClass('no-display');
+        vc5.removeClass('display');
+        
+    }
+
+    if(i==4){
+        img5.addClass('display');
+        img5.removeClass('no-display');
+        img2.addClass('no-display');
+        img2.removeClass('display');
+        img3.addClass('no-display');
+        img3.removeClass('display');
+        img4.addClass('no-display');
+        img4.removeClass('display');
+        img1.addClass('no-display');
+        img1.removeClass('display');
+
+        vc5.addClass('display');
+        vc5.removeClass('no-display');
+        vc2.addClass('no-display');
+        vc2.removeClass('display');
+        vc3.addClass('no-display');
+        vc3.removeClass('display');
+        vc4.addClass('no-display');
+        vc4.removeClass('display');
+        vc1.addClass('no-display');
+        vc1.removeClass('display');   
+    }
+    
+    var timeout = setTimeout(cleanClasses, 500);
+
+});
+
+function cleanClasses() {
+    if(paginationWrapper.hasClass('transition-next')) {
+      paginationWrapper.removeClass('transition-next')
+    } else if(paginationWrapper.hasClass('transition-prev')) {
+      paginationWrapper.removeClass('transition-prev')
+    }
+  }
+
+
+
+
+
--- index.php
+++ index.php
@@ -8,19 +8,97 @@
     <div class="photo-container">
     <div class="container-image img1 display">
       <div class="screen-index"></div>
-      <video playsinline loop muted autoplay src="Videos/tennis1.mp4"></video>
+      <video playsinline loop muted autoplay src="Videos/pro.mp4"></video>
     </div>
     <div class="container-image img2 no-display">
       <div class="screen-index"></div>
-      <video playsinline loop muted autoplay src="Videos/tennis2.mp4"></video>
+      <video playsinline loop muted autoplay src="Videos/tennis3.mp4"></video>
     </div>
     <div class="container-image img3 no-display">
       <div class="screen-index"></div>
-      <video playsinline loop muted autoplay src="Videos/tennis3.mp4"></video>
+      <video playsinline loop muted autoplay src="Videos/sparring.mp4"></video>
+    </div>
+    <div class="container-image img4 no-display">
+      <div class="screen-index"></div>
+      <video playsinline loop muted autoplay src="Videos/tennis1.mp4"></video>
+    </div>
+    <div class="container-image img5 no-display">
+      <div class="screen-index"></div>
+      <video playsinline loop muted autoplay src="Videos/tennis2.mp4"></video>
     </div>
 
     <div class="page-container">
+
+      <!-- SLIDE 1 -->
       <div class="valeurs-container valeurs-container-1 display">
+        <div class="valeurs-texte">
+          <h2> Jouer avec un PRO </h2>
+          <p> Progressez rapidement en jouant avec un de nos joueurs  pros OTT ! Avec la formule O'TT 1000 TRAINING PRO , vivez le rêve de jouer avec l’un de nos joueurs pros  étant ou ayant été sur le circuit , venez profiter de son expérience du plus haut niveau mondial, des coulisses de l’ATP ou WTA  ! </p>
+        </div>
+
+        <div class="flex-btn-accueil">
+          <a href="cours-individuels.php" class="btn-link">
+            <div class="button-container">
+              <h2> Réserver </h2>
+            </div>
+          </a>
+
+          <a href="equipe.php" class="btn-link">
+            <div class="button-container-hollow">
+              <h2> Voir les pros </h2>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <!-- SLIDE 2 -->
+      <div class="valeurs-container valeurs-container-2 no-display">
+        <div class="valeurs-texte">
+          <h2> Coaching individuel </h2>
+          <p> Mettez toutes les chances de votre côté et optez pour des cours sur mesure avec un coaching individuel. A Paris, tout au long de l’année, un entraînement individuel vous est proposé en fonction de vos disponibilités. L’entraînement avec le coach de tennis professionnel est à la fois méthodique et rigoureux.
+          </p>
+        </div>
+        <div class="flex-btn-accueil">
+          <a href="cours-individuels.php" class="btn-link">
+            <div class="button-container">
+              <h2> Réserver </h2>
+            </div>
+          </a>
+
+          <a href="equipe.php" class="btn-link">
+            <div class="button-container-hollow">
+              <h2> Voir les coachs </h2>
+            </div>
+          </a>
+        </div>
+      </div>
+
+
+      <!-- SLIDE 3 -->
+      <div class="valeurs-container valeurs-container-3 no-display">
+        <div class="valeurs-texte">
+          <h2> Jouer avec un sparring </h2>
+          <p> Progressez rapidement en jouant avec un de nos sparrings OTT tous classés entre 3/6 et Numéroté français ! 
+            Jouez avec un véritable métronome , idéal pour se régler ou travailler un coup spécifique !
+          </p>
+        </div>
+        <div class="flex-btn-accueil">
+          <a href="cours-individuels.php" class="btn-link">
+            <div class="button-container">
+              <h2> Réserver </h2>
+            </div>
+          </a>
+
+          <a href="equipe.php" class="btn-link">
+            <div class="button-container-hollow">
+              <h2> Voir les sparrings </h2>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <!-- SLIDE 4 -->
+      <div class="valeurs-container valeurs-container-4 no-display">
         <div class="valeurs-texte">
           <h2> Stage de tennis </h2>
           <p> Vivez l’expérience O’TENNIS  TOUR cet été !
@@ -34,7 +112,9 @@
           </div>
         </a>
       </div>
-      <div class="valeurs-container valeurs-container-2 no-display">
+
+      <!-- SLIDE 5 -->
+      <div class="valeurs-container valeurs-container-5 no-display">
         <div class="valeurs-texte">
           <h2> Infrastructures </h2>
           <p> 
@@ -47,18 +127,6 @@
           </div>
         </a>
       </div>
-      <div class="valeurs-container valeurs-container-3 no-display">
-        <div class="valeurs-texte">
-          <h2> Coaching Individuel</h2>
-          <p> Mettez toutes les chances de votre côté et optez pour des cours sur mesure avec un coaching individuel. A Paris, tout au long de l’année, un entraînement individuel vous est proposé en fonction de vos disponibilités. L’entraînement avec le coach de tennis professionnel est à la fois méthodique et rigoureux.
-          </p>
-        </div>
-          <a href="cours-individuels.php" class="btn-link">
-            <div class="button-container">
-              <h2> Trouver ma leçon </h2>
-            </div>
-          </a>
-      </div>
       
     
       <div class="pagination-wrapper">
@@ -68,7 +136,7 @@
         </svg>
         
         <div class="pagination-container">
-          <h2> <span class='carrousel-number'>01 / 03</span> </h2>
+          <h2> <span class='carrousel-number'>01 / 05</span> </h2>
         </div>
         
         <svg class="button_accueil btn--next" height="96" viewBox="0 0 24 24" width="96" xmlns="http://www.w3.org/2000/svg">
@@ -79,7 +147,36 @@
     </div>
   </div>
 </div>
-<script src="JS/index3.js?v=<?php echo time(); ?>"></script>
+<script src="JS/page-changer.js?v=<?php echo time(); ?>"></script>
 
 </body>
 </html>
+
+<!-- <div class="valeurs-container valeurs-container-1 display">
+        <div class="valeurs-texte">
+          <h2> Stage de tennis </h2>
+          <p> Vivez l’expérience O’TENNIS  TOUR cet été !
+          Reservez votre stage en 2022 et rejoignez l’aventure.
+          De 10 à 18 ans, quelque soit votre niveau, profitez durant les semaines de vacances d’un entraînement personnalisé et adapté afin de maximiser votre potentiel et d'atteindre vos objectifs .
+          </p>
+        </div>
+        <a href="inscriptions.php" class="btn-link">
+          <div class="button-container">
+            <h2> Trouver mon stage </h2>
+          </div>
+        </a>
+      </div> -->
+
+      <!-- <div class="valeurs-container valeurs-container-2 no-display">
+        <div class="valeurs-texte">
+          <h2> Infrastructures </h2>
+          <p> 
+            Pour votre séjour de tennis, O’TENNIS TOUR vous accueille dans les CREPS (Centres de ressources, d'expertise et de performance sportive) de Toulouse, Boulouris Saint Raphaël , Poitiers ainsi que sur la base de loisirs d’Hostens (Landes Girondines). 
+          </p>
+        </div>
+        <a href="inscriptions.php" class="btn-link">
+          <div class="button-container">
+            <h2> Trouver mon stage </h2>
+          </div>
+        </a>
+      </div> -->
