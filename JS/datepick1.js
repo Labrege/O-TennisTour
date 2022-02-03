751a62db31b65424a6dea847678d9751c1b9bd94
mevla124 <65461902+mevla124@users.noreply.github.com>
2022-02-03 02:07:29 +0100

    Update datepick1.js

--- JS/datepick1.js
+++ JS/datepick1.js
@@ -1,19 +1,23 @@
 var today = new Date();
 var tomorrow = new Date();
+var calculateTmr = new Date();
 
 console.log(today.getMonth()+1);
 console.log(((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(today.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000));
 
-// calculateTmr.setDate(today.getDate() + 3);
-// if (calculateTmr.getDay() == 0){
-//   console.log('Sunday')
-// }
+calculateTmr.setDate(today.getDate() + 3);
+if (calculateTmr.getDay() == 0){
+  tomorrow.setDate(today.getDate() + 4);
+}
 
-// if (calculateTmr.getDay() == 6){
-//   console.log('Saturday')
-// }
+else if (calculateTmr.getDay() == 6){
+  tomorrow.setDate(today.getDate() + 5);
+}
 
-tomorrow.setDate(today.getDate() + 3);
+else{
+  tomorrow.setDate(today.getDate() + 3);
+}
+
 var lastMonth = new Date().getMonth() - 1;
 
 var frDate = {
@@ -40,8 +44,7 @@ var picker = new Pikaday({
     dateDayNumb = ((Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000)
     todayDayNumb = ((Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()) - Date.UTC(today.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000)
     totalDif = dateDayNumb - todayDayNumb
-    if (totalDif<7 && (date.getDay() == 0 || date.getDay() == 6)){ // (date.getDay() == 0 || date.getDay() == 6)
-      if()  
+    if (totalDif<7 && (date.getDay() == 0 || date.getDay() == 6)){ // (date.getDay() == 0 || date.getDay() == 6) 
       return true;
     }
     else{
