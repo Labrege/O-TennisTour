c31f776df500a28ebf0256e97115b1220785c407
mevla124 <65461902+mevla124@users.noreply.github.com>
2022-09-21 07:29:02 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,16 @@
 
 <?php
 
-$servername = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "ott";
-
-
 // $servername = "localhost";
-// $dbusername = "u303072403_solal";
-// $dbpassword = "OvalXV75016";
-// $dbname = "u303072403_ott";
+// $dbusername = "root";
+// $dbpassword = "";
+// $dbname = "ott";
+
+
+$servername = "localhost";
+$dbusername = "u303072403_solal";
+$dbpassword = "OvalXV75016";
+$dbname = "u303072403_ott";
 
 
 $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
